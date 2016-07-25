96b0d1fe5029ea8bbbbe00d5ea79ce12fea8e2b1
Rob Hudson <rhudson@weddingwire.com>
2016-07-25 10:40:05 -0400

    Add Allergies exercise

---
+++ exercises/allergies/AllergiesExample.m
@@ -0,0 +1,29 @@
+#import "AllergiesExample.h"
+
+@interface Allergies ()
+
+@property (nonatomic, assign) int score;
+
+@end
+
+@implementation Allergies
+
+- (instancetype)initWithScore:(int)score {
+    self = [super init];
+    
+    if (self) {
+        self.score = score;
+    }
+    
+    return self;
+}
+
+- (BOOL)hasAllergy:(Allergen)allergen {
+    if ((allergen & self.score) == allergen) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+@end
