3871e7ad4e7f748e8bc27e1d2b248e395664a0cb
Rob Hudson <robkhudson@gmail.com>
2017-03-28 14:27:49 -0400

    Clean up Sublist exercise and fix warnings

--- exercises/sublist/SublistTest.m
+++ exercises/sublist/SublistTest.m
@@ -10,107 +10,104 @@
 
 @end
 
-
 @implementation SublistTest
 
 - (void)testEmptyLists {
-    XCTAssertEqualObjects(@"equal", [SublistExample classifierForFirstList:@[] andSecondList:@[]]);
+    XCTAssertEqual(SublistKindEqual, [SublistExample classifierForFirstList:@[] andSecondList:@[]]);
 }
     
 - (void)testEmptyListWithinNonEmptyList {
     NSArray *secondList = @[@1, @2, @3];
-    XCTAssertEqualObjects(@"sublist", [SublistExample classifierForFirstList:@[] andSecondList:secondList]);
-
+    XCTAssertEqual(SublistKindSublist, [SublistExample classifierForFirstList:@[] andSecondList:secondList]);
 }
     
 - (void)testNonEmptyListContainsEmptyList {
     NSArray *firstList = @[@1, @2, @3];
-    XCTAssertEqualObjects(@"superlist", [SublistExample classifierForFirstList:firstList andSecondList:@[]]);
+    XCTAssertEqual(SublistKindSuperlist, [SublistExample classifierForFirstList:firstList andSecondList:@[]]);
 }
     
 - (void)testListEqualsItself {
     NSArray *firstList = @[@1, @2, @3];
     NSArray *secondList = @[@1, @2, @3];
-    XCTAssertEqualObjects(@"equal", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindEqual, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testDifferentLists {
     NSArray *firstList = @[@1, @2, @3];
     NSArray *secondList = @[@2, @3, @4];
-    XCTAssertEqualObjects(@"unequal", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindUnequal, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testFalseStart {
     NSArray *firstList = @[@1, @2, @5];
     NSArray *secondList = @[@0, @1, @2, @3, @1, @2, @5, @6];
-    XCTAssertEqualObjects(@"sublist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSublist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testConsecutive {
     NSArray *firstList = @[@1, @1, @2];
     NSArray *secondList = @[@0, @1, @1, @1, @2, @1, @2];
-    XCTAssertEqualObjects(@"sublist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSublist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testSublistAtStart {
     NSArray *firstList = @[@0, @1, @2];
     NSArray *secondList = @[@0, @1, @2, @3, @4, @5];
-    XCTAssertEqualObjects(@"sublist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSublist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testSublistInMiddle{
-    
     NSArray *firstList = @[@2, @3, @4];
     NSArray *secondList = @[@0, @1, @2, @3, @4, @5];
-    XCTAssertEqualObjects(@"sublist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSublist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testSublistAtEnd {
     NSArray *firstList = @[@3, @4, @5];
     NSArray *secondList = @[@0, @1, @2, @3, @4, @5];
-    XCTAssertEqualObjects(@"sublist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSublist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testAtStartOfSuperlist {
     NSArray *firstList = @[@0, @1, @2, @3, @4, @5];
     NSArray *secondList = @[@0, @1, @2];
-    XCTAssertEqualObjects(@"superlist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSuperlist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testAtMiddleOfSuperlist {
     NSArray *firstList = @[@0, @1, @2, @3, @4, @5];
     NSArray *secondList = @[@2, @3];
-    XCTAssertEqualObjects(@"superlist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSuperlist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
 
 - (void)testAtEndOfSuperlist {
     NSArray *firstList = @[@0, @1, @2, @3, @4, @5];
     NSArray *secondList = @[@3, @4, @5];
-    XCTAssertEqualObjects(@"superlist", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindSuperlist, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testFirstListMissingElementFromSecondList {
     NSArray *firstList = @[@1, @3];
     NSArray *secondList = @[@1, @2, @3];
-    XCTAssertEqualObjects(@"unequal", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindUnequal, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testSecondListMissingOneElementFromFirstList {
     NSArray *firstList = @[@1, @2, @3];
     NSArray *secondList = @[@1, @3];
-    XCTAssertEqualObjects(@"unequal", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindUnequal, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testOrderMattersToAList {
     NSArray *firstList = @[@1, @2, @3];
     NSArray *secondList = @[@3, @2, @1];
-    XCTAssertEqualObjects(@"unequal", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindUnequal, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
     
 - (void)testSameDigitsButDifferentNumbers {
     NSArray *firstList = @[@1, @0, @1];
     NSArray *secondList = @[@10, @1];
-    XCTAssertEqualObjects(@"unequal", [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
+    XCTAssertEqual(SublistKindUnequal, [SublistExample classifierForFirstList:firstList andSecondList:secondList]);
 }
 
 @end
