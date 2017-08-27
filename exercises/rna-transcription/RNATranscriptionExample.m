5111615d1ec45dd23e4b661f8e0cc0b7b2cdab09
Sam Krishna <sam.krishna@gmail.com>
2017-08-27 07:25:29 -0700

    Removed complex version of transcription method

--- exercises/rna-transcription/RNATranscriptionExample.m
+++ exercises/rna-transcription/RNATranscriptionExample.m
@@ -4,11 +4,6 @@
 
 - (NSString *)rnaFromDNAStrand:(NSString *)dnaStrand
 {
-    return [self rnaFromDNAStrandSimple:dnaStrand];
-}
-
-- (NSString *)rnaFromDNAStrandSimple:(NSString *)dnaStrand
-{
     NSDictionary *dnaToRNADict = @{ @"G" : @"C",
                                     @"C" : @"G",
                                     @"T" : @"A",
@@ -25,47 +20,6 @@
     }
     
     return [rnaStrand copy];
-}
-
-- (NSString *)rnaFromDNAStrandComplex:(NSString *)dnaStrand
-{
-    NSDictionary *dnaToRNADict = @{ @"G" : @"C",
-                                    @"C" : @"G",
-                                    @"T" : @"A",
-                                    @"A" : @"U" };
-    
-    NSArray *dnaKeys = [dnaToRNADict allKeys];
-    NSString *dnaKeyString = [dnaKeys componentsJoinedByString:@""];
-    NSCharacterSet *invertedSet = [[NSCharacterSet characterSetWithCharactersInString:dnaKeyString] invertedSet];
-    NSRange invertedSetRange = [dnaStrand rangeOfCharacterFromSet:invertedSet];
-    if (invertedSetRange.location != NSNotFound) {
-        return nil;
-    }
-    
-    NSMutableString *rna = [NSMutableString stringWithString:dnaStrand];
-    NSMutableDictionary *dnaToRnaRanges = [NSMutableDictionary dictionary];
-    
-    [[dnaToRNADict allKeys] enumerateObjectsUsingBlock:^(NSString *dnaKey, NSUInteger idx, BOOL * _Nonnull stop) {
-        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:dnaKey options:0 error:NULL];
-        NSMutableArray *ranges = [NSMutableArray array];
-        
-        [regex enumerateMatchesInString:dnaStrand options:0 range:NSMakeRange(0, [dnaStrand length]) usingBlock:^(NSTextCheckingResult * _Nullable match, NSMatchingFlags flags, BOOL * _Nonnull stop) {
-            NSRange range = [match rangeAtIndex:0];
-            [ranges addObject:[NSValue valueWithRange:range]];
-        }];
-        
-        dnaToRnaRanges[dnaKey] = [ranges copy];
-    }];
-    
-    [dnaToRNADict enumerateKeysAndObjectsUsingBlock:^(NSString *dnaKey, NSString *rnaValue, BOOL * _Nonnull stop) {
-        NSArray *dnaRanges = dnaToRnaRanges[dnaKey];
-        
-        [dnaRanges enumerateObjectsUsingBlock:^(NSValue *dnaRangeValue, NSUInteger idx, BOOL * _Nonnull stop) {
-            [rna replaceOccurrencesOfString:dnaKey withString:rnaValue options:NSLiteralSearch range:[dnaRangeValue rangeValue]];
-        }];
-    }];
-    
-    return [rna copy];
 }
 
 @end
